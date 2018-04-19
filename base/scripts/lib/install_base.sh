e258100489ebe705445dd5018199a5bdcb49e36b
vonrussel <vonrussel@users.noreply.github.com>
2018-04-19 22:56:38 +0800

    Update install_base.sh

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -2,3 +2,15 @@
 set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential g++ python git
+
+# https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-in-docker
+apt-get update && apt-get install -yq libgconf-2-4
+apt-get update && apt-get install -y wget --no-install-recommends \
+  && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+  && apt-get update \
+  && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
+  --no-install-recommends \
+  && rm -rf /var/lib/apt/lists/* \
+  && apt-get purge --auto-remove -y curl \
+  && rm -rf /src/*.deb
