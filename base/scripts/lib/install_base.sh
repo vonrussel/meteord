2ce619079a918baa6c37519cf35f63a9e5650be4
vonrussel <vonrussel@users.noreply.github.com>
2018-04-19 23:15:47 +0800

    Update install_base.sh

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -10,7 +10,4 @@ apt-get update && apt-get install -y wget --no-install-recommends \
   && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
   && apt-get update \
   && apt-get install -y google-chrome-unstable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst ttf-freefont \
-  --no-install-recommends \
-  && rm -rf /var/lib/apt/lists/* \
-  && apt-get purge --auto-remove -y curl \
-  && rm -rf /src/*.deb
+  --no-install-recommends
